174bc1cef75ecbf9706858537867739ce0d949de
eduardolopsl <edulop777@gmail.com>
2024-06-15 23:34:12 +0200

    chore(config): changed vite config for including necessary scss files in project

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,25 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), eslint()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+        @import "@/assets/styles/scss/_fonts.scss";
+        @import "@/assets/styles/scss/_variables.scss";
+        @import "@/assets/styles/scss/_mixin.scss";
+        `,
+      },
     },
   },
 })
